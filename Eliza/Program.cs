29730df4a9eb7851d677558921fa977373993437
Patience Muru <murup@my.lanecc.edu>
2023-05-12 23:23:52 -0700

    finishing up the midterm applications

---
+++ Eliza/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("> ");
+            string input = Console.ReadLine();
+            if (input == "exit") break;
+            string response = CreateElizaResponse(input);
+            Console.WriteLine(response);
+        }
+    }
+
+    static string CreateElizaResponse(string statement)
+    {
+        if (statement.Contains("my"))
+        {
+            int startIndex = statement.IndexOf("my") + 3;
+            int endIndex = statement.IndexOf(" ", startIndex);
+            if (endIndex == -1) endIndex = statement.Length;
+            string noun = statement.Substring(startIndex, endIndex - startIndex);
+            return "Tell me more about your " + noun;
+        }
+        else if (statement.Contains("love") || statement.Contains("hate"))
+        {
+            return "You have strong feelings about that!";
+        }
+        else
+        {
+            string[] responses = { "Please go on.", "Tell me more.", "Continue." };
+            return responses[new Random().Next(responses.Length)];
+        }
+    }
+
+}
---
+++ palindrome/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+
+        public static bool IsPalindrome(string input)
+        {
+            // Clean up the input string by removing whitespace, punctuation, and converting to lowercase
+            string cleanedInput = new string(input.Where(char.IsLetterOrDigit).Select(char.ToLower).ToArray());
+
+            Stack<char> stack = new Stack<char>();
+            Queue<char> queue = new Queue<char>();
+
+            // Add each character in the cleaned input string to the stack and the queue
+            foreach (char c in cleanedInput)
+            {
+                stack.Push(c);
+                queue.Enqueue(c);
+            }
+
+            // Remove each character from the stack and the queue in the natural order and compare
+            while (stack.Count > 0)
+            {
+                if (stack.Pop() != queue.Dequeue())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word or phrase: ");
+            string input = Console.ReadLine();
+
+            if (IsPalindrome(input))
+            {
+                Console.WriteLine("The text you entered is a palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("The text you entered is NOT a palindrome.");
+            }
+        }
+
+    }
+
+}
