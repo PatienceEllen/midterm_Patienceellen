300535678671efb5556a048d7ab08e4ccbf90159
Patience Muru <murup@my.lanecc.edu>
2023-05-12 23:10:49 -0700

    Professor

---
+++ Professor/EmptyClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace CommunityCollege
+{
+    class Professor
+    {
+        private string lNumber;
+        private string firstName;
+        private string lastName;
+        private string department;
+
+        public string LNumber
+        {
+            get { return lNumber; }
+            set
+            {
+                if (value.StartsWith("L") && value.Length == 9)
+                {
+                    lNumber = value;
+                }
+                else
+                {
+                    throw new ArgumentException("LNumber should start with L and be 9 characters long");
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+        public Professor()
+        {
+
+        }
+
+        public Professor(string lNumber, string firstName, string lastName, string department)
+        {
+            LNumber = lNumber;
+            FirstName = firstName;
+            LastName = lastName;
+            Department = department;
+        }
+
+        public override string ToString()
+        {
+            return $"{LNumber} {FirstName} {LastName} ({Department})";
+        }
+    }
+}
+
---
+++ Professor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CommunityCollege
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Professor professor = new Professor("L12345678", "fritz", "herbert", "psychology");
+            Console.WriteLine(professor.ToString());
+
+            professor.FirstName = "Potato";
+            Console.WriteLine(professor.ToString());
+
+            try
+            {
+                professor.LNumber = "123456789";
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
